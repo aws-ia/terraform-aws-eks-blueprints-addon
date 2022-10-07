dec3e91b1e629b7b812c068211840b9f8f227f2b
Bryant Biggs <bryantbiggs@gmail.com>
2022-10-07 14:22:32 -0400

    chore: Update configurations using [`terraform-aws-eks-blueprints`](https://github.com/aws-ia/terraform-aws-eks-blueprints) as baseline

---
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,19 @@
+### What does this PR do?
+
+🛑 Please open an issue first to discuss any significant work and flesh out details/direction - we would hate for your time to be wasted.
+Consult the [CONTRIBUTING](https://github.com/aws-ia/terraform-aws-eks-addon/.github/blob/main/CONTRIBUTING.md#contributing-via-pull-requests) guide for submitting pull-requests.
+
+<!-- A brief description of the change being made with this pull request. -->
+
+### Motivation
+
+<!-- What inspired you to submit this pull request? -->
+- Resolves #<issue-number>
+
+### Test Results
+
+<!-- Please provide supporting evidence and steps taking to test and validation this change -->
+
+### Additional Notes
+
+<!-- Anything else we should know when reviewing? -->
---
+++ examples/complete/main.tf
@@ -0,0 +1,107 @@
+provider "aws" {
+  region = local.region
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.this.token
+  }
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = module.eks.cluster_id
+}
+
+data "aws_availability_zones" "available" {}
+
+locals {
+  name   = basename(path.cwd)
+  region = "us-west-2"
+
+  vpc_cidr = "10.0.0.0/16"
+  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  tags = {
+    Example    = local.name
+    GithubRepo = "aws-ia/terraform-aws-eks-addon"
+  }
+}
+
+################################################################################
+# EKS Addon
+################################################################################
+
+# module "eks_addon" {
+#   source = "../../"
+
+#   cluster_id                = module.eks.cluster_id
+#   cluster_oidc_provider_arn = module.eks.oidc_provider_arn
+
+#   tags = local.tags
+# }
+
+################################################################################
+# Supporting resources
+################################################################################
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 18.30"
+
+  cluster_name    = local.name
+  cluster_version = "1.23"
+
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+  control_plane_subnet_ids = module.vpc.intra_subnets
+
+  eks_managed_node_groups = {
+    initial = {
+      instance_types        = ["m5.large"]
+      create_security_group = false
+
+      min_size     = 1
+      max_size     = 5
+      desired_size = 2
+    }
+  }
+
+  tags = local.tags
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 3.0"
+
+  name = local.name
+  cidr = local.vpc_cidr
+
+  azs             = local.azs
+  private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k)]
+  public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 48)]
+  intra_subnets   = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 52)]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  # Manage so we can name
+  manage_default_network_acl    = true
+  default_network_acl_tags      = { Name = "${local.name}-default" }
+  manage_default_route_table    = true
+  default_route_table_tags      = { Name = "${local.name}-default" }
+  manage_default_security_group = true
+  default_security_group_tags   = { Name = "${local.name}-default" }
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+
+  tags = local.tags
+}
